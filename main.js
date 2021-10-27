3529566f7e405be85596d7e5abe39fa9e6a3fd87
Stas Ganiev <stas@ganiev.pro>
2021-10-28 08:52:41 +1000

    lessons 0.2.3-4

--- main.js
+++ main.js
@@ -6,4 +6,66 @@
 
 // lesson 0.2.2
 
-console.log("Я программирую на JavaScript\nИ это моя первая программа");
+//console.log("Я программирую на JavaScript\nИ это моя первая программа");
+
+// lesson 0.2.3
+
+/*
+var old; // устарело
+let a;
+a = 5;
+console.log("a");
+console.log(a);
+a = "Hello";
+console.log(a);
+let name = "Alex";
+console.log(name);
+let age = 18.7, single = true;
+let str_1 = `${name}, вам ${age} лет`;
+console.log(str_1);
+console.log(name + ", вам " + age + " лет");
+
+let inf = 1 / 0;
+console.log(inf);
+
+let nan = "abc" * 5;
+console.log(nan);
+
+let und;
+console.log(und);
+
+let n = null;
+console.log(n);
+
+console.log(typeof(single));
+*/
+
+// домашка
+
+let age = 40;
+let name = "Стас";
+let single = false;
+
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(single));
+
+console.log(`Меня зовут ${name}, мне ${age} лет, моё семейное положение ${single}`);
+
+// lesson 0.2.4
+
+/*
+const PI = 3.1415926;
+console.log(PI);
+let r = 5;
+let area = PI * r * r;
+console.log(`Площать окружности с радиусом ${r} равна ${area}`);
+*/
+
+// домашка
+
+const G = 9.8;
+let m = 80;
+let h = G * m;
+
+console.log(h);
