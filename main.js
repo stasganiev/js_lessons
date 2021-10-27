ec241d7fe0810cf181a620d17b6049941ad59e22
Stas Ganiev <stas@ganiev.pro>
2021-10-28 08:21:56 +1000

    lessons 0.2.1-2

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// lesson 0.2.1
+
+//alert("Я программирую на JavaScript\nИ это моя первая программа");
+
+// lesson 0.2.2
+
+console.log("Я программирую на JavaScript\nИ это моя первая программа");
