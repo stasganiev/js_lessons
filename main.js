7e1f56b8973d437a58aaad4efd5072468103210c
Stas Ganiev <stas@ganiev.pro>
2021-10-29 08:55:59 +1000

    lessons 0.2.5-6

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ console.log(typeof(single));
 
 // домашка
 
+/*
 let age = 40;
 let name = "Стас";
 let single = false;
@@ -51,6 +52,7 @@ console.log(typeof(name));
 console.log(typeof(single));
 
 console.log(`Меня зовут ${name}, мне ${age} лет, моё семейное положение ${single}`);
+*/
 
 // lesson 0.2.4
 
@@ -64,8 +66,87 @@ console.log(`Площать окружности с радиусом ${r} рав
 
 // домашка
 
+/*
 const G = 9.8;
 let m = 80;
 let h = G * m;
 
 console.log(h);
+*/
+
+// lesson 0.2.5
+/*
+let str = "12.5";
+console.log(typeof(str));
+let number = Number(str);
+console.log(typeof(number) + " " + number);
+console.log("abc");
+console.log(typeof(true) + " " + typeof(false));
+
+console.log("1" + "2");
+console.log(Number("1") + Number("2"));
+console.log(3 + " number");
+console.log(3 + 2 * 4);
+
+console.log(String(true) + " " + typeof(String(true)));
+console.log(String(15.7) + " " + typeof(String(15.7)));
+
+console.log(Boolean(15))
+
+let a = "15";
+let b = "17";
+console.log(+a + +b);
+*/
+
+// домашка
+/*
+let a = "12.5";
+let b = "-5.7";
+console.log(a + b);
+
+a = Number(a);
+b = Number(b);
+console.log(a + b);
+*/
+
+//lesson 0.2.6
+
+/*
+let a = 15.7;
+let b = 25;
+console.log("a = " + a);
+console.log("b = " + b);
+
+a = -a;
+console.log("-a = " + a);
+
+a = 10;
+b = 4;
+
+console.log(`${a} % ${b} = ${a % b}`);
+console.log(`${a} ** ${b} = ${a ** b}`);
+
+a++;
+b--;
+console.log(`${a} + ${b} = ${a + b}`);
+
+a = a + 10;
+a += 10;
+*/
+
+// домашка
+
+let r = (8 + 15) / ((14 - 9) * (7.8 - 15 * 14));
+console.log(r);
+
+let a = 8 + 15;
+let b = (14 - 9);
+let c = 7.8 - 15 * 14;
+
+r = a / (b * c);
+console.log(r);
+
+let s1 = "12";
+let s2 = "40";
+console.log(s1 + s2);
+console.log(Number(s1) + Number(s2));
