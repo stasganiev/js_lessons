f22e1e010234938c0f24ab738810b43819326555
Stas Ganiev <stas@ganiev.pro>
2021-11-15 09:19:17 +1000

    lesson 0.2.20 calculation

---
+++ js/functions.js
@@ -0,0 +1,44 @@
+function checkNumber(n) {
+    let num = Number(n);
+    if (Number.isNaN(num)) {
+        console.log(`Введено некорректное значение ${n}`);
+        return false;
+    }
+    return true;
+}
+
+function sum(x, y) {
+    let ch1 = checkNumber(x);
+    let ch2 = checkNumber(y);
+
+    if (!ch1 || !ch2) return NaN;
+    
+    return +x + +y;
+}
+
+function sub(x, y) {
+    let ch1 = checkNumber(x);
+    let ch2 = checkNumber(y);
+
+    if (!ch1 || !ch2) return NaN;
+    
+    return +x - +y;
+}
+
+function mult(x, y) {
+    let ch1 = checkNumber(x);
+    let ch2 = checkNumber(y);
+
+    if (!ch1 || !ch2) return NaN;
+    
+    return +x * +y;
+}
+
+function div(x, y) {
+    let ch1 = checkNumber(x);
+    let ch2 = checkNumber(y);
+
+    if (!ch1 || !ch2 || +y === 0) return NaN;
+    
+    return +x / +y;
+}
