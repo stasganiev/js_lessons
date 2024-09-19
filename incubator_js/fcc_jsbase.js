72cfea934177f18172c13bf917928ccafbe245e4
Stas <stas@ganiev.pro>
2024-09-19 15:44:45 +0200

    pop method

--- incubator_js/fcc_jsbase.js
+++ incubator_js/fcc_jsbase.js
@@ -10,7 +10,15 @@ console.log(rows);
 
 rows[rows.length-1] = 10;
 
+//Step 26
 let cities = ["London", "New York", "Mumbai"];
 console.log(cities);
 cities[cities.length - 1] = "Mexico City";
 console.log(cities);
+
+//Step 27
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+let popped = rows.pop();
+console.log(popped);
