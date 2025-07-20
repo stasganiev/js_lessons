da1b1150ee7384d36632d11d7d47ddb2a2076869
Stas Ganiev <stas@ganiev.pro>
2025-07-20 06:30:58 +0200

    Human choice

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -10,6 +10,25 @@ function getComputerChoice() {
     return 'Scissors';
 }
 
+function getHumanChoice() {
+    let choiceValue = +prompt('1 - Rock, 2 - Paper, 3 - Scissors');
+    if (choiceValue === 1) {
+        return 'Rock';
+    } else if (choiceValue === 2) {
+        return 'Paper';
+    } else if (choiceValue === 3) {
+        return 'Scissors';
+    }
+    return undefined;
+}
+
+let humanChoice = undefined;
+
+while (humanChoice === undefined) {
+    humanChoice = getHumanChoice();
+}
+
 let computerChoice = getComputerChoice();
 
-console.log(computerChoice);
+console.log(humanChoice);
+console.log(computerChoice);4
