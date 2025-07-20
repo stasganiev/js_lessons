6cc3e6cc7367cd831b2e63beef2c0d806390f4a2
Stas Ganiev <stas@ganiev.pro>
2025-07-20 06:15:19 +0200

    Computer's choicing

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('Hello, world!');
+function getComputerChoice() {
+    let choiceValue = Math.random();
+    if (choiceValue < 0.333) {
+        return 'Rock';
+    } else if (choiceValue >= 0.666) {
+        return 'Paper';
+    }
+    return 'Scissors';
+}
+
+let computerChoice = getComputerChoice();
+
+console.log(computerChoice);
