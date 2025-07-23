33b380b1fb83090370a777c5d7970424a668c3b9
Stas Ganiev <stas@ganiev.pro>
2025-07-23 07:13:06 +0200

    Arrays, task 2 - filter ramge

--- js/arrays.js
+++ js/arrays.js
@@ -2,20 +2,37 @@
 
 const innputStr01 = document.querySelector('.input_str01-input');
 const btnCamelize = document.querySelector('.btn_camelize');
+const btnFilterRng = document.querySelector('.btn_filterRng');
 
-btnCamelize.addEventListener('click', function(evt) {
-
-  evt.preventDefault();
-
-  let src = innputStr01.value;
-
-  let terms = src.split('-');
+function camelize(str) {
+    
+  let terms = str.split('-');
   let resArray = terms.map((item, index) => {
     if (index === 0) return item.toLowerCase();
     else return item.slice(0, 1).toUpperCase() + item.slice(1).toLowerCase();
   });
-  let result = resArray.join('');
 
-  outputResult(result);
+  return resArray.join('');
+
+}
+
+function filterRange(arr, a, b) {
+
+    return arr.filter(item => +item >= a && +item <= b);
+
+}
+
+btnCamelize.addEventListener('click', function(evt) {
+
+  evt.preventDefault();
+  outputResult(camelize(innputStr01.value));
+
+});
+
+btnFilterRng.addEventListener('click', function(evt) {
+
+  evt.preventDefault();
+  let res = filterRange(innputStr01.value.split('-'), +innputNum1.value, +innputNum2.value);
+  outputResult(res);
 
 });
