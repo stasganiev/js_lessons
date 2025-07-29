880f4fd259366c77f535655886e98874a1792ccf
Stas Ganiev <stas@ganiev.pro>
2025-07-29 06:38:12 +0200

    feat: step 3 - canvas resize

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,18 +1,49 @@
 'use strict';
 
+// Variables
+
 let containerItem = document.querySelector('.container');
 let itemTemplate = document.createElement('div');
 itemTemplate.classList.add('item');
 
+const btnSize = document.querySelector('.btn_size');
+
 let ind;
 let currentItem;
 
-for (ind = 0; ind < 256; ind++) {
-    currentItem = itemTemplate.cloneNode(true);
-    containerItem.appendChild(currentItem);
+// Handlers & Functions
 
-    currentItem.addEventListener('mouseenter', (evt) => {
-        evt.preventDefault();
-        evt.target.style.backgroundColor = '#777';
-    })
+const updateCanvas = (size) => {
+
+    const totalCells = size * size;
+    const canvasSize = 800;
+    containerItem.innerHTML = '';
+
+    for (ind = 0; ind < totalCells; ind++) {
+        currentItem = itemTemplate.cloneNode(true);
+        currentItem.style.width = (canvasSize / size) + 'px';
+        currentItem.style.height = (canvasSize / size) + 'px';
+        containerItem.appendChild(currentItem);
+        currentItem.addEventListener('mouseenter', onOverview);
+    }
+
 }
+
+const onOverview = (evt) => {
+    evt.preventDefault();
+    evt.target.style.backgroundColor = '#777';
+}
+
+const btnResizeOnClick = () => {
+    let cellCount = +prompt('Your count (7 to 100)', 16);
+    while (cellCount < 7 || cellCount > 100) {
+        cellCount = +prompt('Your count (7 to 100)', cellCount);
+    }
+    updateCanvas(cellCount);
+}
+
+// Main code
+
+btnSize.addEventListener('click', btnResizeOnClick);
+
+updateCanvas(16);
