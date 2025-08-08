5678fd558309793a02f50be5cac7db317cdc8b10
Stas Ganiev <stas@ganiev.pro>
2025-08-08 08:32:52 +0200

    feat: day 2

--- Js_gpt/week_01.js
+++ Js_gpt/week_01.js
@@ -16,3 +16,30 @@ if (age < 18 ) {
 let res = 0;
 for (let i = 1; i <= 100; i++) res += i;
 console.log(res);
+
+// Day 2
+//
+// Task 1
+
+function getGreet(name) {
+  return `Hello, ${name}`;
+}
+
+// Task 2 + 3
+
+let arrMovies = ['Devil wares Prado', 'Like Mike', 'Always say Yes', 'Star Wars', 'Harry Potter'];
+
+function outputArray(arr) {
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${(i+1)}. ${arr[i]}`);
+  }
+
+  for (let item of arr) {
+    console.log(item);
+  }
+}
+
+outputArray(arrMovies);
+arrMovies.push('Terminator');
+outputArray(arrMovies);
